41d05e798ad29923bb28a71486463bd55e2560da
Pradeesh <pradeesh@gmail.com>
2022-08-14 10:42:35 +1200

    added documentation test

--- app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
+++ app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
@@ -2,6 +2,10 @@ package com.example.kotlinhelloworld
 
 class MathAdder {
 
+    /**
+     * Calculates two numbers
+     * @return sum of two numbers
+     */
     fun add(a: Int, b: Int): Int {
         //return 0;
         return a + b
