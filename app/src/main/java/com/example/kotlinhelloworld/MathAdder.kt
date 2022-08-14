d257f7d5d6337b83a81d775a8c1c36c76afeab8e
Pradeesh <pradeesh@gmail.com>
2022-08-14 15:34:59 +1200

    added dokka stuff and fix the build test

--- app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
+++ app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
@@ -4,10 +4,9 @@ class MathAdder {
 
     /**
      * Calculates two numbers
-     * @return sum of two numbers
      */
     fun add(a: Int, b: Int): Int {
         //return 0;
-        return a - b
+        return a + b
     }
 }
