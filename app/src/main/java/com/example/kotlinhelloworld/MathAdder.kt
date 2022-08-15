5a64242bd0bd9003f3a106cfd35f6a2de34609c6
Pradeesh <pradeesh@gmail.com>
2022-08-15 13:23:30 +1200

    update the math adder to be more kotlin friendly doc

--- app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
+++ app/src/main/java/com/example/kotlinhelloworld/MathAdder.kt
@@ -1,9 +1,18 @@
 package com.example.kotlinhelloworld
 
+
+/**
+ * A group of *mathematical operation*.
+ *
+ * This class has no useful logic; it's just a documentation example.
+ * @constructor Creates an empty group.
+ */
+
 class MathAdder {
 
     /**
      * Calculates two numbers
+     * @return the sum of two numbers
      */
     fun add(a: Int, b: Int): Int {
         //return 0;
